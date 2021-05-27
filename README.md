cf763f15634ab886f517506d7a9c2b31564707e6
sukumar <tensiletechofficial@gmail.com>
2021-05-27 21:43:09 +0530

    adding improved UI for form and single page, full website options

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# IID
-An application to get the href list, assets list and download all images
+# Interface.ai Image Downloader
+An application to get the href list, assets list and download all assets in img tag.
+
+# setup
+The application runs in any server capable of hosting php pages.
+For a local setup 
+1.install xammp in your system 
+2.Place the code inside xammp/htdocs/IID
+3.Start apache server on xampp 
+4.open http://localhost/IID on browser
+
+# scope of application
+
+1.This is a php based application that can be used to download assets from a single page or a website.
+
+2.The application tries to download all the assets insde the src of all img tags in a page.
+
+3.The assets that cant be shown on browser can be seen in downloads folder
+
+
+# Scenarios
+1.The application can handles cases like absolute and relative path variations.
+2.The application can differentiate and filter between internal and external links to only download content from internal links without repetition.
+
+
+# unaddressed issues
+
+1.base64 conversions, encrypted, other exceptions that might have caused failure in downloads
+
+# Scope for Improvement 
+
+1.Handling all errors.
+2.user interface for end to end management of all the collected assets. 
--- index.php
+++ index.php
@@ -1,17 +1,63 @@
 <?php
+error_reporting(0);
+ini_set('display_errors', 0);
 include('./simple_html_dom.php');
 //include_path('C:\xampp\php\PEAR');
 ?>
+
+<style>
+input[type=text], select {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+
+input[type=submit] {
+  width: 100%;
+  background-color: #4CAF50;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+input[type=submit]:hover {
+  background-color: #45a049;
+}
+
+div {
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+}
+</style>
+
+<div>
+<img style="height: 100px; margin-left: 45%;"src="./logo.jpg"/>
+<h1 style="text-align:center;width:100%">Interface Image Downloader</h1>
+<h3 style="text-align:center;width:100%;color:gray">Download All assets from any url</h3>
 <form name="urlform"method="post" action=""> 
     
                       
-                       <input  type="text" name="url"/> 
+                       <input  type="text" width="600px" name="url" required/> <br>
+                       <input type="radio"  name="target" value="page">
+  <label for="female">Download Assets from only given url</label>&nbsp;&nbsp;
+                       <input type="radio"  name="target" value="website">
+  <label for="male">Download Assets from entire Website</label><br>
+  
+ 
        
          
-                        <input type="submit" name="submit" id="button" value="Submit" />
+                        <input type="submit" name="submit" value="Submit" />
        
 </form>
-
+</div>
 <?php
 function fetch($url){
     $html=file_get_html($url);
@@ -21,7 +67,7 @@ function fetch($url){
     foreach($a as $anc){
 
         if(strpos($anc, $url) !== false){
-               echo $anc->href.'<br>';
+              // echo $anc->href.'<br>';
                array_push($urllist, $anc->href);
         }
 
@@ -32,12 +78,12 @@ function fetch($url){
                 if(substr($anc->href, 0,1) == "/" && substr( $url,-1) == "/"){
                     
                                 $urlnew=  substr($url, 0, -1);
-                                 echo $urlnew.$anc->href.'<br>';
+                                // echo $urlnew.$anc->href.'<br>';
                                  array_push($urllist, $urlnew.$anc->href);
 
                 }
                 else{
-                    echo $url.$anc->href.'<br>';
+                    //echo $url.$anc->href.'<br>';
                     array_push($urllist, $url.$anc->href);
 
                 }
@@ -47,7 +93,7 @@ function fetch($url){
 
             if(substr($anc->href, 0,1) != "/" && substr( $url,-1) != "/")
             {
-                echo $url.'/'.$anc->href.'<br>';
+              //  echo $url.'/'.$anc->href.'<br>';
                 array_push($urllist, $url.'/'.$anc->href);
                      
             }
@@ -62,26 +108,43 @@ function fetch($url){
       
 
      }
+     echo "<b>Pages list Scanned ✔ </b><br>";
+     echo "<b>Downloading assets from pages..... </b><br>";
+     
+
+     
+
+     foreach(array_unique($urllist) as $page){
+         echo $page.'<br>';
+     }
 
     return array_unique($urllist);
 
 }
+
+
+
+
 function getWebsiteAssets($url){
 
-    $urllist = fetch($url);
-     print_r($urllist);
-
-     foreach($urllist as $page){
+    //$urllist = fetch($url);
+    
+    // print_r($urllist);
+    // echo "<b>Pages list Scanned ✔ <b><br>";
+   
+    // foreach($pages as $page){
+        $page =$url;
         $html=file_get_html($page);
         $image = $html->find("img");
+        echo "<h1>Downloaded Images from".$page."</h1>";
         foreach($image as $img) {
                     //Get attribute of images
                     
                     $souceImg=$img->src;
-            $imgName = 'Downloads/'.basename($souceImg);
+                    $imgName = 'Downloads/'.basename($souceImg);
             
             
-            if(strpos($souceImg, $page) !== false){
+                if(strpos($souceImg, 'http') !== false){
                 file_put_contents($imgName, file_get_contents($souceImg));
                 echo '<img src="'.$imgName.'" height="100px" />';
             }
@@ -94,17 +157,25 @@ function getWebsiteAssets($url){
             
                   }
 
-     }
+                
+
+    // }
+    echo "<h1>For more check in Downloads folder</h1>";
 }
 
 
 if(isset($_POST["submit"])){
 
-   getWebsiteAssets($_POST["url"]);
+   
 
-     
-
-
-
+   if( $_POST["target"] == 'website'){
+    $urllist= fetch($_POST["url"]);
+    foreach($urllist as $url){
+        getWebsiteAssets($url);
     }
+   }
+   else{
+    getWebsiteAssets($_POST["url"]);
+   }
+}
     ?>
